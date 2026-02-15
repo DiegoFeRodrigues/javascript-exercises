b4477cf05f501c3f30303fb9ca6a97399049ff90
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-15 13:48:13 -0300

    PPT - Início da função de uma partida
    Máquina: Pedra, Jogador: Tesoura = Máquina vence

--- foundations/06.1_Pedra-Papel-Tesoura/javascript.js
+++ foundations/06.1_Pedra-Papel-Tesoura/javascript.js
@@ -19,10 +19,26 @@ function escolhaJogador() {
 let pontosMaquina = 0
 let pontosJogador = 0
 
+let maquina = escolhaMaquina()
+let jogador = escolhaJogador()
+
+function partida(maquina, jogador) {
+    pedraTesoura = "\nPedra amassa tesoura."
+    maquinaVence = " Máquina vence a rodada"
+    switch (maquina) {
+        case ("Pedra"):
+            if (jogador == "Tesoura") {
+                return pedraTesoura + maquinaVence
+            }
+    }
+}
+
 console.log("Pedra, papel e Tesoura \n\n")
 console.log("Jogadas:")
-console.log("Máquina:", escolhaMaquina())
-console.log("Jogador:", escolhaJogador())
+console.log("Máquina:", maquina)
+console.log("Jogador:", jogador)
 console.log("\nPlacar:")
 console.log("Pontuação da máquina:", pontosMaquina)
 console.log("Pontuação do jogador:", pontosJogador)
+
+console.log(partida(maquina, jogador))
