14122ca119b58dafb75688d4f6a1a22535462bec
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-15 12:13:13 -0300

    Pedra, papel e tesoura - Função escolha da máquina

--- foundations/06.1_Pedra-Papel-Tesoura/javascript.js
+++ foundations/06.1_Pedra-Papel-Tesoura/javascript.js
@@ -1,3 +1,15 @@
 
 
-console.log("INICIANDO...")
+function escolhaMaquina() {
+    maquinaEscolheu = Math.floor(Math.random() * 3)
+    switch (maquinaEscolheu) {
+        case 0:
+            return "Pedra"
+        case 1:
+            return "Papel"
+        case 2:
+            return "Tesoura"
+    }
+}
+console.log("Pedra, papel e Tesoura \n\n")
+console.log("Jogada da máquina:", escolhaMaquina())
