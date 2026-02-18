b7d1339372a4b9d8d85c8cd5842973405e58b3e7
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-18 15:30:53 -0300

    String invertida - Quarto teste - Funciona com strings vazias

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -6,5 +6,6 @@ const reverseString = function(texto) {
 console.log(reverseString("hello"));
 console.log(reverseString("hello there"));
 console.log(reverseString("123! abc! Hello, Odinite."))
+console.log(reverseString(""));
 // Do not edit below this line
 module.exports = reverseString;
