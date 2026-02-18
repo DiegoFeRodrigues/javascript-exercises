0b0ea7d308480be665b7721999b801c8b2dd8947
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-18 15:21:43 -0300

    Exercícios JavaScript - String invertida
    Primeiro teste - Retorna só uma string invertida

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(texto) {
+    arrayInvertido = texto.split("").reverse();
+    return arrayInvertido.join("")
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
