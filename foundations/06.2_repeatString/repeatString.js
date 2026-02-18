c898c4aef051047519e1e45ca9938e251049b357
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-18 11:59:45 -0300

    Exercícios JavaScript - Arrays
    Primeiro exercício - Repete string

--- foundations/06.2_repeatString/repeatString.js
+++ foundations/06.2_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
 
-};
-
+const lista = [];
+const repeatString = function(texto, numero) {
+    for (let cont = 0; cont < numero; cont++) {
+        lista.push(texto); 
+    }
+    return lista.join("");
+}
+console.log(repeatString('hey', 3));
 // Do not edit below this line
 module.exports = repeatString;
