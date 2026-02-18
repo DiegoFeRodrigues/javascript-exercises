19573f0e54fc01653cfc9b638a2f1160b2c6ea7b
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-18 16:19:13 -0300

    Exercícios JavaScript - Remove dados de lista
    Remove apenas UM dado de lista

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(lista, excluirDado) {
+    lista.splice(lista.indexOf(excluirDado), 1);
+    return lista;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
