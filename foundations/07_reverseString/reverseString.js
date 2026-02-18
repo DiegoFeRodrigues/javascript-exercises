a0c18ca69f67f56bc9fba1b32ad327d58df3f261
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-18 15:29:44 -0300

    String invertida - Terceiro teste - Funciona com números e pontuação

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -5,6 +5,6 @@ const reverseString = function(texto) {
 
 console.log(reverseString("hello"));
 console.log(reverseString("hello there"));
-
+console.log(reverseString("123! abc! Hello, Odinite."))
 // Do not edit below this line
 module.exports = reverseString;
