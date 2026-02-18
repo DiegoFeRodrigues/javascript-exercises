32fb834b303e6c248cebeae264147e33e0c3ca90
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-18 14:50:01 -0300

    Repete String - Sétimo teste

--- foundations/06.2_repeatString/repeatString.js
+++ foundations/06.2_repeatString/repeatString.js
@@ -1,15 +1,20 @@
 
 const repeatString = function(texto, numero) {
     const lista = [];
-    for (let cont = 0; cont < numero; cont++) {
-        lista.push(texto); 
+    if (numero < 0) {
+        return "ERROR"
+    } else {
+        for (let cont = 0; cont < numero; cont++) {
+            lista.push(texto); 
+        }
+        return lista.join("");
     }
-    return lista.join("");
 }
 console.log(repeatString('hey', 3));
 console.log(repeatString('hello', 10));
 console.log(repeatString('hi', 1));
 console.log(repeatString('bye', 0));
 console.log(repeatString('hey', 3));
+console.log(repeatString('hey', -3));
 // Do not edit below this line
 module.exports = repeatString;
