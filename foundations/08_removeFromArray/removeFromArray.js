a9a759067b0074cb2539838d224f08f37655e58f
DiegoFeRodrigues <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-20 15:39:20 -0300

    Remove da Lista - Passa no teste de funcionar com strings

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -12,6 +12,7 @@ console.log(removeFromArray([1, 2, 2, 3], 2));
 console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
 console.log(removeFromArray([1, 2, 3, 4], 7, 2));
 console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
+console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
