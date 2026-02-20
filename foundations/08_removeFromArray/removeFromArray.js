cbb4df4f79cc43b3a4559c3e5aa5c5aa5512938a
DiegoFeRodrigues <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-20 15:37:55 -0300

    Remove da Lista - Passa no teste de remover todos os valores

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -11,6 +11,7 @@ console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 console.log(removeFromArray([1, 2, 2, 3], 2));
 console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
 console.log(removeFromArray([1, 2, 3, 4], 7, 2));
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
