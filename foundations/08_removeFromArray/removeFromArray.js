cd9d5660a40f02184bb84f10515a287b90640818
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-20 12:36:37 -0300

    Remove de lista - Remove mais de um valor

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
-const removeFromArray = function(lista, excluirDado) {
-    lista.splice(lista.indexOf(excluirDado), 1);
-    return lista;
+const removeFromArray = function(lista, ...excluirDado) {
+    const novaLista = [];
+    for (item of lista) {
+        if (!excluirDado.find((itemAexcluir) => itemAexcluir == item))novaLista.push(item);
+    }
+    return novaLista;
 };
 
 console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
