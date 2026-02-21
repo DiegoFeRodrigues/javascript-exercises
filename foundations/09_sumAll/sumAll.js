697ab0d54a3ad20cad50194dbd6e09e3f7664706
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-21 19:09:49 -0300

    Soma tudo - Passa no quinto teste
    Retorna ERROR se valores não forem inteiros
    Testes são realizados no início, se falso, não executa restante da
    função

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -2,15 +2,18 @@ const sumAll = function(primeiroValor, segundoValor) {
     let soma = 0;
     let maiorValor = 0;
     let menorValor = 0;
-    if (primeiroValor > segundoValor) {
+
+    if (primeiroValor < 0 || segundoValor < 0 || 
+    !Number.isInteger(primeiroValor) || !Number.isInteger(segundoValor)) {
+        return "ERROR";
+    } else if (primeiroValor > segundoValor) {
         menorValor = segundoValor;
         maiorValor = primeiroValor;
-    } else if (primeiroValor < 0 || segundoValor < 0) {
-        return "ERROR";
     } else {
         menorValor = primeiroValor;
         maiorValor = segundoValor;
     }
+
     for (let cont = menorValor; cont <= maiorValor; cont++) {
         soma += cont;
     }
@@ -21,13 +24,7 @@ console.log(sumAll(2, 4));
 console.log(sumAll(1, 4000));
 console.log(sumAll(123, 1));
 console.log(sumAll(-10, 4));
-// console.log();
-// console.log();
-// console.log();
-// console.log();
-// console.log();
-// console.log();
-// console.log();
+console.log(sumAll(2.5, 4));
 // console.log();
 // console.log();
 
