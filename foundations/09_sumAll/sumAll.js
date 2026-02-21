b5ee50b5686211302783f113445df134d0b4ac01
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-21 19:25:40 -0300

    SomaTudo - Passa no sexto e sétimo teste - Retorna ERROR se valores não forem números

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -25,8 +25,8 @@ console.log(sumAll(1, 4000));
 console.log(sumAll(123, 1));
 console.log(sumAll(-10, 4));
 console.log(sumAll(2.5, 4));
-// console.log();
-// console.log();
+console.log(sumAll(10, "90"));
+console.log(sumAll(10, [90, 1]));
 
 // Do not edit below this line
 module.exports = sumAll;
