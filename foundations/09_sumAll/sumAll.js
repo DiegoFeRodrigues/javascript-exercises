d2bc26cd606c2f9cffadcaac98bf5b533c8da2bb
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-21 18:09:20 -0300

    Exercícios JavaScript - Soma tudo
    Passa no primeiro teste
    Utilizei for loop mas deve ter uma solução melhor

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(primeiroValor, segundoValor) {
+    let soma = 0;
+    for (let cont = primeiroValor; cont <= segundoValor; cont++) {
+        soma += cont;
+    }
+    return soma;
 };
+
+console.log(sumAll(2, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
