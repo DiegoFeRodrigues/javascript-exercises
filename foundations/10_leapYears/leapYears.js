57b38f6c0621458ff2ee8a4ccd7a84fd369d0cf4
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-22 12:29:37 -0300

    Exercícios JavaScript - Anos bissextos
    Passa nos testes com anos não seculares

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(ano) {
+    if (ano % 100 !== 0 && ano % 4 === 0) return true;
+    return false;
+    
 };
+
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
 
 // Do not edit below this line
 module.exports = leapYears;
