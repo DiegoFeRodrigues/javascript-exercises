e37925cac0c72d63dd56f5a85c3562641def1924
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-22 18:13:29 -0300

    Exercícios JavaScript - Conversor de temperatura
    Teste inicial - Converte Fahrenheit para Celsius

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatura) {
+  return (temperatura - 32) * 5 / 9;
 };
 
 const convertToFahrenheit = function() {
 };
+
+console.log(convertToCelsius(32));
+// console.log();
 
 // Do not edit below this line
 module.exports = {
