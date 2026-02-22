3f57f7ee2cfe4d3cbba4e2761cd80322ad75d25f
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-22 12:47:38 -0300

    Anos Bissextos - Passa nos testes com anos seculares
    Cálculos em constantes para organização, facilitando a leitura

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,12 +1,26 @@
 const leapYears = function(ano) {
+    const seculo = ano % 100 === 0; 
+    const divisivelPor4 = ano % 4 === 0; 
+    const divisivelPor400 = ano % 400 === 0; 
+
+    return !seculo && divisivelPor4 || 
+            seculo && divisivelPor400 ? true : false;
+
+    /* 
+    outra forma - meio poluída, dificulta a leitura:
     if (ano % 100 !== 0 && ano % 4 === 0) return true;
-    return false;
-    
+    return ano % 100 === 0 && ano % 400 === 0 ? true : false;
+    */
+
 };
 
 console.log(leapYears(1996));
 console.log(leapYears(1997));
 console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(700));
+
 
 // Do not edit below this line
 module.exports = leapYears;
