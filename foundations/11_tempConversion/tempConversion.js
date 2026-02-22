5405aa506eb1421a18000adad09e27d5e2ecf1eb
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-22 18:34:35 -0300

    Conversor de temperatura - Converte para Celsius
    Passa nos testes:
    Retorno arredondado para cima
    Funciona com temperaturas negativas

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,12 +1,15 @@
 const convertToCelsius = function(temperatura) {
-  return (temperatura - 32) * 5 / 9;
+  const conversor = (temperatura - 32) * 5 / 9;
+  return Math.round(conversor * 10) / 10;
 };
 
 const convertToFahrenheit = function() {
 };
 
 console.log(convertToCelsius(32));
-// console.log();
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(-73.3));
+// console.log(convertToCelsius());
 
 // Do not edit below this line
 module.exports = {
