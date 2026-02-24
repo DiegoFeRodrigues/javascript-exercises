7a911d475eae28e30e4082b4f79a58b9732040bd
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-24 01:11:39 -0300

    Manipulando DOM - Título h3 em azul

--- foundations/11.2_ManipulandoDOM/javaScript.js
+++ foundations/11.2_ManipulandoDOM/javaScript.js
@@ -7,10 +7,9 @@ Add the following elements to the container using ONLY JavaScript and the DOM me
 1 - a <p> with red text that says “Hey I’m red!”
 2 - an <h3> with blue text that says “I’m a blue h3!”
 3 - a <div> with a black border and pink background color with the following elements inside of it:
-another <h1> that says “I’m in a div”
-4 - a <p> that says “ME TOO!”
+    another <h1> that says “I’m in a div”
+    a <p> that says “ME TOO!”
 Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
-
 */
 
 const corpoPagina = document.querySelector("#corpo");
@@ -19,5 +18,10 @@ const paragrafoVermelho = document.createElement("p");
 paragrafoVermelho.classList.add("redPara");
 paragrafoVermelho.textContent = "Parágrafo em vermelho."
 paragrafoVermelho.style.color = "red";
-
 corpoPagina.appendChild(paragrafoVermelho);
+
+const tituloH3 = document.createElement("h3");
+tituloH3.classList.add("tituloH3");
+tituloH3.textContent = "Título h3 em azul.";
+tituloH3.style.color = "#0202f6";
+corpoPagina.appendChild(tituloH3);
