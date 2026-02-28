66da77a8e86a84f842b14d175e23d0c42497fb9e
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-02-27 22:20:59 -0300

    Eventos - Anotações e links

--- foundations/11.3_Eventos/03_EventListeners/02_FuncaoNomeada/javaScript.js
+++ foundations/11.3_Eventos/03_EventListeners/02_FuncaoNomeada/javaScript.js
@@ -6,3 +6,45 @@ function funcaoAlerta() {
 const botao = document.querySelector("#botao");
 
 botao.addEventListener("click", funcaoAlerta);
+
+
+/* MAIS SOBRE EVENTOS - ANOTAÇÕES / LINKS: */
+
+// imprime detalhes do evento:
+botao.addEventListener("click", function (e) {
+  console.log(e);
+});
+
+botao.addEventListener("click", function (e) {
+    console.log(e.type);
+});
+
+// exemplo evento mouse:
+botao.addEventListener('wheel', e => console.log(e.deltaY));
+
+// W3Schools JavaScript Event Reference page:
+//     https://www.w3schools.com/jsref/dom_obj_event.asp
+
+// Funções - callbacks:
+    // https://dev.to/i3uckwheat/understanding-callbacks-2o9e
+
+/* Eventos - JavaScript Tutorial:
+
+    Introdução - Event flow, Event bubbling, Event capturing, 
+    DOM level 2 Event flow, Event object:
+        https://www.javascripttutorial.net/javascript-dom/javascript-events/
+
+    Mouse events:
+        https://www.javascripttutorial.net/javascript-dom/javascript-mouse-events/
+
+    Event delegation:
+        https://www.javascripttutorial.net/javascript-dom/javascript-event-delegation/
+
+    Dispatch event:
+        https://www.javascripttutorial.net/javascript-dom/javascript-dispatchevent/
+
+    Custom events:
+        https://www.javascripttutorial.net/javascript-dom/javascript-custom-events/
+
+    
+*/
