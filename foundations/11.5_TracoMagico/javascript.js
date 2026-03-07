e77048366e6ce6264057b56175eb044c4876d1a8
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-03-07 18:33:40 -0300

    Traço Mágico - Iniciando grid de 16 x 16. Adicionada linha com 16 divs

--- foundations/11.5_TracoMagico/javascript.js
+++ foundations/11.5_TracoMagico/javascript.js
@@ -2,18 +2,13 @@
 
 const corpoPagina = document.querySelector("#corpoPagina");
 corpoPagina.style.display = "flex";
+corpoPagina.style.gap = "1px";
 
-const div1 = document.createElement("div");
-div1.style.backgroundColor = "blue";
-
-const div2 = document.createElement("div");
-div2.style.backgroundColor = "black";
-
-corpoPagina.appendChild(div1);
-corpoPagina.appendChild(div2);
-
-const juntaDivs = corpoPagina.querySelectorAll("div");
-juntaDivs.forEach(item => {
-    item.style.height= "16px";
-    item.style.width = "16px";
-});
+for (let linha = 0; linha < 16; linha++) {
+    const divLinha = document.createElement("div");
+    divLinha.style.display = "flex";
+    divLinha.style.backgroundColor = "black";
+    divLinha.style.height = "10px";
+    divLinha.style.width = "10px";
+    corpoPagina.appendChild(divLinha);
+}
