42a161e95c91813b7402e0303fcb5ccbc3e4f19e
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-03-07 19:07:17 -0300

    Traço Mágico - Adicionando colunas para grid de 16 x 16

--- foundations/11.5_TracoMagico/javascript.js
+++ foundations/11.5_TracoMagico/javascript.js
@@ -1,14 +1,20 @@
 
-
 const corpoPagina = document.querySelector("#corpoPagina");
-corpoPagina.style.display = "flex";
-corpoPagina.style.gap = "1px";
+
+const divGrid = document.querySelector(".divGrid");
 
 for (let linha = 0; linha < 16; linha++) {
     const divLinha = document.createElement("div");
     divLinha.style.display = "flex";
-    divLinha.style.backgroundColor = "black";
-    divLinha.style.height = "10px";
-    divLinha.style.width = "10px";
-    corpoPagina.appendChild(divLinha);
+
+    for (let coluna = 0; coluna < 16; coluna++) {
+        const divColuna = document.createElement("div");
+        divColuna.style.backgroundColor = "black";
+        divColuna.style.height = "10px";
+        divColuna.style.width = "10px";
+        divColuna.style.border = "1px solid white";
+        divLinha.appendChild(divColuna);
+    }
+
+    divGrid.appendChild(divLinha);
 }
