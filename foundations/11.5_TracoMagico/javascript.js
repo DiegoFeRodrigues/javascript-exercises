d8a387cfae3d6021a4d749325f4970bb16089e31
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-03-07 12:55:58 -0300

    Traço Mágico - Duas divs iniciais apenas para teste

--- foundations/11.5_TracoMagico/javascript.js
+++ foundations/11.5_TracoMagico/javascript.js
@@ -1,3 +1,19 @@
 
 
 const corpoPagina = document.querySelector("#corpoPagina");
+corpoPagina.style.display = "flex";
+
+const div1 = document.createElement("div");
+div1.style.backgroundColor = "blue";
+
+const div2 = document.createElement("div");
+div2.style.backgroundColor = "black";
+
+corpoPagina.appendChild(div1);
+corpoPagina.appendChild(div2);
+
+const juntaDivs = corpoPagina.querySelectorAll("div");
+juntaDivs.forEach(item => {
+    item.style.height= "16px";
+    item.style.width = "16px";
+});
