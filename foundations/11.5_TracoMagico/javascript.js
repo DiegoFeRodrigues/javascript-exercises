b1bb49a2277a5164b35363438d0b5460dd18ae95
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-03-08 18:05:54 -0300

    Traço Mágico - Função que contrói o grid

--- foundations/11.5_TracoMagico/javascript.js
+++ foundations/11.5_TracoMagico/javascript.js
@@ -4,20 +4,25 @@ corpoPagina.style.fontFamily = "Arial";
 
 const divGrid = document.querySelector(".divGrid");
 
-for (let linha = 0; linha < 16; linha++) {
-    const divLinha = document.createElement("div");
-    divLinha.style.display = "flex";
+const tamanhoGridPadrao = 16;
 
-    for (let coluna = 0; coluna < 16; coluna++) {
-        const divColuna = document.createElement("div");
-        divColuna.style.backgroundColor = "black";
-        divColuna.style.height = "10px";
-        divColuna.style.width = "10px";
-        divColuna.style.border = "1px solid white";
-        divLinha.appendChild(divColuna);
-
-        divColuna.addEventListener("mouseover", () => {divColuna.style.backgroundColor = "white"});
+function grid(tamanho) {
+    for (let linha = 0; linha < tamanho; linha++) {
+        const divLinha = document.createElement("div");
+        divLinha.style.display = "flex";
+    
+        for (let coluna = 0; coluna < tamanho; coluna++) {
+            const divColuna = document.createElement("div");
+            divColuna.style.backgroundColor = "black";
+            divColuna.style.height = "10px";
+            divColuna.style.width = "10px";
+            divColuna.style.border = "1px solid white";
+            divLinha.appendChild(divColuna);
+    
+            divColuna.addEventListener("mouseover", () => {divColuna.style.backgroundColor = "white"});
+        }
+        divGrid.appendChild(divLinha);
     }
-
-    divGrid.appendChild(divLinha);
 }
+
+grid(tamanhoGridPadrao);
