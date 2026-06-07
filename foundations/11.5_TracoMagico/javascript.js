80018cb1e8066e3d57d3ac868f8cc1fcc3daeac8
Diego Ferreira <237453864+DiegoFeRodrigues@users.noreply.github.com>
2026-06-07 01:56:45 -0300

    Consertando divGrid e botãoTamanho
    Estava usando duas divs para criação do grid
    Agora é criada apenas uma (divGrid), dentro da função grid
    Também consertada o eventListener do botaoTamanho. Agora ao clicar,
    remove a divGrid antiga (que é criada novamente na chamada da função
    grid) e ele funciona continuamente

--- foundations/11.5_TracoMagico/javascript.js
+++ foundations/11.5_TracoMagico/javascript.js
@@ -12,21 +12,7 @@ corpoPagina.style.backgroundColor = "lightGrey";
 
 const msgUsuario = document.querySelector(".msgUsuario");
 
-const divGrid = document.querySelector(".divGrid");
-divGrid.style.maxHeight = "745px";
-divGrid.style.maxWidth = "745px";
-divGrid.style.overflow = "hidden";
-divGrid.style.display = "flex";
-divGrid.style.flexDirection = "column";
-divGrid.style.alignItems = "center";
-divGrid.style.marginTop = "10px";
-divGrid.style.border = "10px double black";
-
 const tamanhoGridPadrao = 16;
-
-const divGridPadrao = document.createElement("div");
-divGridPadrao.classList.add("gridPadrao");
-divGrid.appendChild(divGridPadrao);
 
 function nAleatorio(number) {
   return Math.floor(Math.random() * (number + 1));
@@ -35,6 +21,19 @@ function nAleatorio(number) {
 function grid(tamanho) {
 
     let opacidadeInicialQuadrados = 1;
+
+    const divGrid = document.createElement("div");
+    divGrid.classList = "divGrid";
+    divGrid.style.maxHeight = "745px";
+    divGrid.style.maxWidth = "745px";
+    divGrid.style.overflow = "hidden";
+    divGrid.style.display = "flex";
+    divGrid.style.flexDirection = "column";
+    divGrid.style.alignItems = "center";
+    divGrid.style.marginTop = "10px";
+    divGrid.style.border = "10px double black";
+
+    corpoPagina.appendChild(divGrid);
 
     for (let linha = 0; linha < tamanho; linha++) {
         const divLinha = document.createElement("div");
@@ -55,11 +54,7 @@ function grid(tamanho) {
                 divColuna.style.opacity = opacidadeInicialQuadrados -= 0.1;
             });
         }
-        if (tamanho == tamanhoGridPadrao) {
-            divGridPadrao.appendChild(divLinha);
-        } else {
-            divGrid.appendChild(divLinha);
-        }
+        divGrid.appendChild(divLinha);
     }
 }
 
@@ -73,8 +68,12 @@ botaoTamanho.style.color = "lightGrey";
 botaoTamanho.style.fontSize = "16px";
 botaoTamanho.style.fontWeight = "700";
 botaoTamanho.style.border = "1px solid darkGrey"
+
 botaoTamanho.addEventListener("click", () => {
-    divGrid.removeChild(divGridPadrao);
+    
+    const divGridAntiga = document.querySelector(".divGrid");
+    corpoPagina.removeChild(divGridAntiga);
+
     let tamanhoEscolhido = prompt("Informe o tamanho desejado (máximo é 100):")
     while (tamanhoEscolhido > 100) {
         tamanhoEscolhido = prompt("O tamanho máximo é 100. Digite o tamanho desejado:")
@@ -83,6 +82,5 @@ botaoTamanho.addEventListener("click", () => {
     msgUsuario.appendChild(msgTamanhoEscolhido);
     msgTamanhoEscolhido.textContent = `Quadro com tamanho ${tamanhoEscolhido}px x ${tamanhoEscolhido}px:`
     document.querySelectorAll("p").forEach(para => msgUsuario.removeChild(para));
-    corpoPagina.removeChild(document.querySelector("button"));
     grid(tamanhoEscolhido);
 })
